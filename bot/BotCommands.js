ec5de919b8c1036d817b24fe78e89c36ab0ecfa6
Isabele Ketchup <garotaketchupgirl@gmail.com>
2016-10-28 17:18:09 -0200

    Add nocolor command

--- bot/BotCommands.js
+++ bot/BotCommands.js
@@ -1,10 +1,12 @@
 const Command = require('../slave/Command.js');
 const Colours = require('../commands/colours.js');
 const MyColours = require('../commands/mycolours.js');
+const NoColour = require('../commands/nocolour.js');
 
 const BotCommands = {
     Colours: new Command(Colours),
     MyColours: new Command(MyColours),
+    NoColour: new Command(NoColour),
 }
 
 module.exports = BotCommands;
