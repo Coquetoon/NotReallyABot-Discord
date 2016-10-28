594c5a5e6cc2635f104d6edc45577dc8f80b5b8a
Isabele Ketchup <garotaketchupgirl@gmail.com>
2016-10-28 03:25:24 -0200

    Add config.js example

---
+++ config.example.js
@@ -0,0 +1,24 @@
+const Config = {};
+
+// Prefix of the command
+Config.prefix = 'COMMAND PREFIX';
+// Should I ignore the handling of my own messages?
+Config.ignoringMyself = true;
+
+// Messages displayes by the bot upon ready.
+// Message showed on console.
+Config.readyConsole = 'Ready.'
+// Message showed on a chat room.
+Config.ready = [
+    {
+        'id': 'textChannel id',
+        'message': 'Ready to be a bot',
+    },
+];
+
+// Owners of the bot
+Config.masters = [
+    'id',
+]
+
+module.exports = Config;
