7f1bda1b971c5131002eb5d15980b699175fc093
Coquetoon <coque@ketche.net>
2016-10-28 17:49:25 -0200

    Add colour command

--- commands/colour.js
+++ commands/colour.js
@@ -1,10 +1,26 @@
 const ColourFilter = require('../plugins/colorize/ColourFilter.js');
 
+function normalize(name) {
+    return name.replace(/_/g, '').toLowerCase().match(/\w+/g).join('');
+}
+
 function Colour(message, question, client) {
     if (message.guild === undefined) return;
 
-    filter = ColourFilter.filter(message.member.roles);
+    if (question.args === undefined) {
+        message.reply('You must specify a colour.').then(m => m.delete(6 * 1000));
+        return;
+    }
 
+    requestedColourName = normalize(question.args);
+
+    filter = ColourFilter.filter(message.guild.roles);
+    role = filter.find(role => normalize(role.name) == requestedColourName);
+    
+    if (role == undefined) {
+        message.reply('The colour `' + question.args + '` does not exist.')
+        return;
+    }
     
 }
 
